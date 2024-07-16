4abca68b4910e6675917e1ff3f1f4071aed9d212
Rex Yu <yu19910513@gmail.com>
2024-07-16 04:31:56 -0700

    update CA and connection file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,29 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const fs = require('fs');
+
+// Load the CA certificate
+const caCert = fs.readFileSync('config/us-east-1-bundle.pem');
 
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
-    pool: { max: 500, min: 0, idle: 10000, acquire: 100*1000 }
-  })
+      pool: { max: 500, min: 0, idle: 10000, acquire: 100 * 1000 },
+      dialectOptions: {
+        ssl: {
+          ca: caCert
+        }
+      }
+    })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
       dialectOptions: {
         decimalNumbers: true,
-      },
+        ssl: {
+          ca: caCert
+        }
+      }
     });
 
 module.exports = sequelize;
